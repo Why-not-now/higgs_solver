c412b0f437809431e35a7e955802bf19e229a82b
Why-not-now <81090788+Why-not-now@users.noreply.github.com>
2022-10-29 02:23:00 +0800

    creation of project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='Mushroom Boi',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
