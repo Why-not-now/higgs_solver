a33a643e26fc23e6f563c3011b426b0d3ef51863
Why-not-now <81090788+Why-not-now@users.noreply.github.com>
2022-10-29 02:25:49 +0800

    Finally corrected fully

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='funniest',
+setup(name='Higgs Solver',
       version='0.1',
-      description='The funniest joke in the world',
+      description='Solver for the app Agent Higgs of testtubegames.com',
       url='http://github.com/Why-not-now/higgs_solver',
       author='Why-not-now',
       license='MIT',
-      packages=['funniest'],
+      packages=['higgs_solver'],
       zip_safe=False)
