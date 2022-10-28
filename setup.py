a0ed4bb2c1ed71308c2a65e4e1fd5af95ca368e9
Why-not-now <81090788+Why-not-now@users.noreply.github.com>
2022-10-29 02:24:08 +0800

    setup corrected

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name='funniest',
       version='0.1',
       description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
-      author='Mushroom Boi',
+      url='http://github.com/Why-not-now/higgs_solver',
+      author='Why-not-now',
       license='MIT',
       packages=['funniest'],
       zip_safe=False)
