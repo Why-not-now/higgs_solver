6ea42333d0357625394ee7cfa87749e731177ad3
Why-not-now <81090788+Why-not-now@users.noreply.github.com>
2023-07-01 14:15:17 +0800

    new file:   Setup.py 	new file:   higgs_solver/__init__.py Create project

---
+++ Setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='higgs_solver',
+      version='0.1',
+      description='Solves the upcoming game Agent Higgs 2 (insert link here)',
+      url='',
+      author='Growing Mycelium',
+      author_email='theseriouslyunknownsnake@gmail.com',
+      license='MIT',
+      packages=['higgs_solver'],
+      zip_safe=False)
