6f37277a0930b3179efdc336c12c514413d9c2f5
Why-not-now <81090788+Why-not-now@users.noreply.github.com>
2023-07-04 21:48:37 +0800

    version updated

--- src/higgs_solver/__init__.py
+++ src/higgs_solver/__init__.py
@@ -1,1 +1,79 @@
-__version__ = "0.0.0"
+from dataclasses import dataclass, field
+from typing import Optional, TypeAlias
+
+DirectionFilter: TypeAlias = tuple[int, ...]
+LeftFilter: TypeAlias = DirectionFilter
+RightFilter: TypeAlias = DirectionFilter
+UpFilter: TypeAlias = DirectionFilter
+DownFilter: TypeAlias = DirectionFilter
+GeneralFilter: TypeAlias = \
+    tuple[LeftFilter, RightFilter, UpFilter, DownFilter]
+
+
+def empty_particles_board(width: int, height: int) -> tuple[None, ...]:
+    return tuple(None for _ in range(width * height))
+
+
+@dataclass(frozen=True)
+class Board():
+    """generic board for game"""
+    width: int
+    height: int
+    particles: tuple[Optional[int], ...] = field(compare=False)
+    _particles_set: frozenset[int]
+    # TODO: replace with Particles
+
+    def left(self, particles) -> "Board":
+        raise NotImplementedError
+        return Board(self.width, self.height, *particles._left(self))
+
+    def right(self, particles) -> "Board":
+        raise NotImplementedError
+        return Board(self.width, self.height, *particles._right(self))
+
+    def up(self, particles) -> "Board":
+        raise NotImplementedError
+        return Board(self.width, self.height, *particles._up(self))
+
+    def down(self, particles) -> "Board":
+        raise NotImplementedError
+        return Board(self.width, self.height, *particles._down(self))
+    
+
+def new_board(
+        width: int,
+        height: int,
+        particles: Optional[tuple[Optional[int], ...]],
+        _particles_set: Optional[frozenset[int]],
+) -> Board:
+    if particles is None:
+        particles = empty_particles_board(width, height)
+
+    if _particles_set is None:
+        _particles_mutable_set: list[int] = \
+            [elem for elem in particles if elem is not None]
+        _particles_set = frozenset(_particles_mutable_set)
+
+    return Board(width, height, particles, _particles_set)
+
+
+def straight_filter(width: int, height: int) \
+        -> tuple[GeneralFilter, ...]:
+    """Returns an object that itself returns the horizontal and vertical
+directional filter for a given position"""
+
+    lookup_table: list[GeneralFilter] = []
+
+    for y in range(0, height * width, width):
+        for x in range(width):
+            left: LeftFilter = \
+                tuple(reversed(range(y, y + x)))
+            right: RightFilter = \
+                tuple(range(y + x + 1, y + width))
+            up: UpFilter = \
+                tuple(reversed(range(x, y + x, width)))
+            down: DownFilter = \
+                tuple(range(y + x + width, width * height, width))
+            lookup_table.append((left, right, up, down))
+
+    return tuple(lookup_table)
---
+++ src/higgs_solver/logic.py
@@ -0,0 +1,29 @@
+# pre movement
+
+# TODO: direction
+# TODO:     strong force (attraction)
+# TODO:     electromagnetic repulsion/attraction
+
+# TODO: decay??? (how to create particle)
+
+# movement
+
+# TODO: particle collision
+
+# TODO: obstacle collision
+
+# TODO: obstacle destruction
+
+# TODO: breaking holes
+
+# TODO: holes
+
+# TODO: strong force (attraction)
+
+# post movement
+
+# TODO: light/heavy annihilation (check connected components)
+
+# TODO: hadron formation (which way to combine?)
+
+# TODO: nucleus formation
